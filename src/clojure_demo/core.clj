2501034c7670a2b55b6c91991e8a4381d8c94935
Tony Deng <wolf.deng@gmail.com>
2016-07-20 15:36:38 +0800

    添加中国数字转阿拉伯数字测试

--- src/clojure_demo/core.clj
+++ src/clojure_demo/core.clj
@@ -5,3 +5,30 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+
+(def zh-arab {"零" 0 "一" 1 "二" 2 "三" 3 "四" 4 "五" 5
+              "六" 6 "七" 7 "八" 8 "九" 9 "十" 10 "百" 100})
+
+;清理数据
+
+(defn clear-data [s]
+  (remove nil? (map zh-arab (clojure.string/split s #""))))
+
+;两个元素求和
+
+(defn sum-two [s]
+  (cond
+    (or (= 10 (last s))
+      (= 100 (last s))) (reduce * (first s) (next s))
+    :else (reduce + (first s) (next s))
+    ))
+
+(defn chinese-number [s]
+  (loop [result 0
+         element (clear-data s)]
+    (if (empty? element)
+      result
+      (recur (+ result (sum-two (take 2 element))) (drop 2 element)
+        ))))
--- test/clojure_demo/core_test.clj
+++ test/clojure_demo/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [clojure-demo.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest chinese-number-test
+  (testing "Test Chinese Number."
+    (= 0   (chinese-number "零"))
+    (= 1   (chinese-number "一"))
+    (= 10  (chinese-number "十"))
+    (= 12  (chinese-number "十二"))
+    (= 20  (chinese-number "二十行"))
+    (= 34  (chinese-number "三十四"))
+    (= 567 (chinese-number "五百六十七"))
+    (= 809 (chinese-number "八百零九回"))
+    (print (chinese-number "九百九十五"))
+    ))
+
